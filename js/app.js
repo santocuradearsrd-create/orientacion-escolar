8c031a2dece82850c38568939b9b58d3221f0bac
santocuradearsrd-create <santocuradearsrd@gmail.com>
2026-04-25 11:57:18 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-import { registerView, renderView } from './router.js?v=3';
-import { initNav, updateNav }       from './nav.js?v=3';
+import { registerView, renderView } from './router.js';
+import { initNav, updateNav }       from './nav.js';
 
-import { renderInicio }                      from './views/inicio.js?v=3';
-import { renderLogin }                       from './views/login.js?v=3';
-import { renderReporte, renderEnviado }      from './views/reporte.js?v=3';
-import { renderPanel }                       from './views/panel.js?v=3';
-import { vistaCaso }                         from './views/caso.js?v=3';
-import { vistaAdmin }                        from './views/admin.js?v=3';
+import { renderInicio }                      from './views/inicio.js';
+import { renderLogin }                       from './views/login.js';
+import { renderReporte, renderEnviado }      from './views/reporte.js';
+import { renderPanel }                       from './views/panel.js';
+import { vistaCaso }                         from './views/caso.js';
+import { vistaAdmin }                        from './views/admin.js';
 
 registerView('inicio',   renderInicio);
 registerView('login',    renderLogin);
